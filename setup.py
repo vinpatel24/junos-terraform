28b7625e5659d57a064e2a3cbab48ed16952bec6
aburston <aburston@juniper.net>
2025-06-30 16:56:50 +0100

    renamed script

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="Junos Terraform Framework",
     #packages=find_packages(),
     packages=["junosterraform", "jtaf_pyang_plugin", "terraform_provider"],
-    scripts=["junosterraform/jtaf-provider", "jtaf_pyang_plugin/jtaf-pyang-plugindir", "junosterraform/jtaf-junos-xml2tf"],
+    scripts=["junosterraform/jtaf-provider", "jtaf_pyang_plugin/jtaf-pyang-plugindir", "junosterraform/jtaf-xml2tf"],
     package_data = {
         "terraform_provider": [ "*", "netconf/*", "templates/*" ]
     },
